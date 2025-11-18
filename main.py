680f55304728d37f80065621fdd9be93f13f64f0
bsayfiddin2197 <bsayfiddin2197@gmail.com>
2025-11-18 17:53:41 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+1
+"""
+# def salomlash():
+#     soat = int(input("soat ni kirit: "))
+#     daq = int(input("daqiqa krt: "))
+#
+#     if soat > 5 and soat < 12 and daq < 60:
+#         print(f"Xayrli tong, soat: {soat}:{daq}")
+#
+#     elif soat < 19 and soat > 12 and daq < 60:
+#         print(f"Xayrli kun, soat: {soat}:{daq}")
+#
+#     elif soat < 23 and soat > 19 and daq < 60:
+#         print(f"xayrli kech, soat: {soat}:{daq}")
+#
+#     else:
+#         print(f"xatolik bor: soat: {soat}:{daq}")
+#
+# salomlash()
+
+'''
+2
+'''
+
+'''
+3
+'''
+
+"""
+4
+"""
+# def paytn():
+#     soz = "Python "[::-1]
+#     print(soz*3)
+#
+# paytn()
